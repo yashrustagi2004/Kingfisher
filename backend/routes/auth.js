fe3d7679c1a0a69bb4c4a8658ce665879e58d081
yashrustagi2004 <yash.rustagi22@st.niituniversity.in>
2025-03-29 19:45:05 +0530

    minor change

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/User");
+const User = require("../models/user");
 
 // Google user info route
 router.post("/google/userinfo", async (req, res) => {
