045e5f2c255885be8cd6121738b9b64ac0f70c79
Adityachawla1901 <aditya.chawla22@st.niituniversity.in>
2025-04-03 10:52:21 +0530

    tempo

--- backend/models/trusted.js
+++ backend/models/trusted.js
@@ -3,4 +3,4 @@ const TrustedSchema = new mongoose.Schema({
   googleId: {type: String, required: true, unique: true,},
   Domains: {type: [String]}
 })
-module.exports = mongoose.model("TrustedDomains", userSchema);
+module.exports = mongoose.model("TrustedDomains", TrustedSchema);
